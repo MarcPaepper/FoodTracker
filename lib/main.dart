a2f8e6310ba7aaf7157d62a3446ca3fe577a6d36
Marc Päpper <m.paepper@mabinso.com>
2024-01-02 23:58:36 +0100

    Refactor UI theme and add form validation in AddProductView

--- lib/main.dart
+++ lib/main.dart
@@ -6,31 +6,60 @@ import 'views/add_product_view.dart';
 import 'views/stats_view.dart';
 
 void main() {
-	runApp(
-		MaterialApp(
-			title: "Food Tracker",
-			debugShowCheckedModeBanner: false,
-			theme: ThemeData(
-				colorSchemeSeed: Colors.teal,
-				appBarTheme: AppBarTheme(
-					backgroundColor: Colors.teal.shade400,
-					foregroundColor: Colors.white,
-				),
+  runApp(
+    MaterialApp(
+      title: "Food Tracker",
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorSchemeSeed: Colors.teal,
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.teal.shade400,
+          foregroundColor: Colors.white,
+        ),
         textTheme: const TextTheme(
           bodyMedium: TextStyle(color: Colors.grey),
         ),
         inputDecorationTheme: InputDecorationTheme(
+          isDense: true,
           filled: true,
-          fillColor: Colors.grey.shade100,
+          fillColor: Colors.grey.withAlpha(35),
+          enabledBorder: UnderlineInputBorder(
+            borderRadius: BorderRadius.circular(10.0),
+            borderSide: const BorderSide(
+              width: 3,
+              color: Colors.grey
+            )
+          ),
+          focusedBorder: UnderlineInputBorder(
+            borderRadius: BorderRadius.circular(10.0),
+            borderSide: BorderSide(
+              width: 2.5,
+              color: Colors.teal.shade400
+            )
+          ),
+          errorBorder: UnderlineInputBorder(
+            borderRadius: BorderRadius.circular(10.0),
+            borderSide: const BorderSide(
+              width: 2.5,
+              color: Color.fromARGB(211, 154, 32, 32)
+            )
+          ),
+          focusedErrorBorder: UnderlineInputBorder(
+            borderRadius: BorderRadius.circular(10.0),
+            borderSide: BorderSide(
+              width: 3.5,
+              color: Colors.red.shade500
+            )
+          ),
         ),
-			),
-			home: const ProductsView(),
-			routes: {
+      ),
+      home: const ProductsView(),
+      routes: {
         addMealsRoute:		(context) => const AddMealView(),
         productsRoute:	  (context) => const ProductsView(),
         addProductRoute:	(context) => const AddProductView(),
         statsRoute:		  	(context) => const StatsView(),
       }
-		)
-	);
+    )
+  );
 }
--- lib/views/add_product_view.dart
+++ lib/views/add_product_view.dart
@@ -3,75 +3,98 @@ import 'package:food_tracker/data_manager.dart';
 import "dart:developer" as devtools show log;
 
 class AddProductView extends StatefulWidget {
-	const AddProductView({super.key});
+  const AddProductView({super.key});
 
-	@override
-	State<AddProductView> createState() => _AddProductViewState();
+  @override
+  State<AddProductView> createState() => _AddProductViewState();
 }
 
 class _AddProductViewState extends State<AddProductView> {
-	late final TextEditingController _name;
-	
-	@override
-	void initState() {
-		_name = TextEditingController();
-		super.initState();
-	}
-	
-	@override
-	void dispose() {
-		_name.dispose();
-		super.dispose();
-	}
-	
-	@override
-	Widget build(BuildContext context) {
-		return Scaffold(
-			appBar: AppBar(
-				title: const Text("Add product"),
-			),
-			body: FutureBuilder(
-				future: loadData(),
-				builder: (context, snapshot) {
-					switch (snapshot.connectionState) {
-						
-						case ConnectionState.done:
-							return Column(
-								children: [
-									TextField(
-										controller: _name,
-										decoration: const InputDecoration(
-											labelText: "Name"
-										),
-									),
-									TextButton(
-										onPressed: () {
-											final name = _name.text;
-											devtools.log("name $name");
-										},
-										child: const Text("Add")
-									),
-								]
-							);
-						case ConnectionState.none:
-						case ConnectionState.waiting:
-						case ConnectionState.active:
-							return Align(
-								alignment: Alignment.topCenter,
-								child: Padding(
-									padding: const EdgeInsets.all(50.0),
-									child: SizedBox(
-										width: 30,
-										height: 30,
-										child: CircularProgressIndicator(
-											color: Colors.teal.shade500
-										),
-									),
-								)
-							);
-					}
-				}
-			)
-		);
-	}
+  late final TextEditingController _name;
+  
+  @override
+  void initState() {
+    _name = TextEditingController();
+    super.initState();
+  }
+  
+  @override
+  void dispose() {
+    _name.dispose();
+    super.dispose();
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Add product"),
+      ),
+      body: FutureBuilder(
+        future: loadData(),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            
+            case ConnectionState.done:
+              return Padding(
+                padding: const EdgeInsets.all(7.0),
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: TextFormField(
+                        controller: _name,
+                        decoration: const InputDecoration(
+                          labelText: "Name"
+                        ),
+                        validator: (String? value) {
+                          devtools.log("hey");
+                          for (var prod in getProducts()) {
+                            if (prod.name == value) {
+                              return "Already taken";
+                            }
+                          }
+                          return null;
+                        },
+                        autovalidateMode: AutovalidateMode.onUserInteraction,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: TextButton(
+                        onPressed: () {
+                          final name = _name.text;
+                          devtools.log("name $name");
+                        },
+                        child: const Text("Add")
+                      ),
+                    ),
+                  ]
+                ),
+              );
+            case ConnectionState.none:
+            case ConnectionState.waiting:
+            case ConnectionState.active:
+              return Align(
+                alignment: Alignment.topCenter,
+                child: Padding(
+                  padding: const EdgeInsets.all(50.0),
+                  child: SizedBox(
+                    width: 30,
+                    height: 30,
+                    child: CircularProgressIndicator(
+                      color: Colors.teal.shade500
+                    ),
+                  ),
+                )
+              );
+          }
+        }
+      )
+    );
+  }
+}
+
+// check whether name is unique
+void checkName(String name) {
 }
