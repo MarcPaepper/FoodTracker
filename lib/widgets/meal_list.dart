a1485c791ab52c1ec9c49644a3cabd9e4f49f0a4
Marc Päpper <m.paepper@mabinso.com>
2024-07-13 16:26:52 +0200

    s

--- lib/widgets/meal_list.dart
+++ lib/widgets/meal_list.dart
@@ -1,6 +1,7 @@
 import "package:flutter/material.dart";
 
 import "../services/data/data_objects.dart";
+import "../services/data/data_service.dart";
 import "../utility/text_logic.dart";
 import "add_meal_box.dart";
 
@@ -21,6 +22,8 @@ class MealList extends StatefulWidget {
 }
 
 class _MealListState extends State<MealList> {
+  final DataService dataService = DataService.current();
+  
   @override
   Widget build(BuildContext context) {
     return ListView(
@@ -34,47 +37,33 @@ class _MealListState extends State<MealList> {
           productsMap: widget.productsMap ?? {},
         ),
         const SizedBox(height: 5),
-        getMealTiles(context, widget.productsMap, widget.meals),
+        ...getMealTiles(context, dataService, widget.productsMap, widget.meals),
       ],
     );
   }
 }
 
-Widget getMealTiles(BuildContext context, Map<int, Product>? productsMap, List<Meal> meals) {
-  List<Widget> children = [];
-  
-  DateTime lastHeader = DateTime(0);
-  for (int i = meals.length - 1; i >= 0; i--) {
+List<Widget> getMealTiles(BuildContext context, DataService dataService, Map<int, Product>? productsMap, List<Meal> meals) {
+  List<Widget> children = [];                                                                                                         
+  DateTime lastHeader = meals.isNotEmpty
+    ? DateTime(meals[0].dateTime.year, meals[0].dateTime.month, meals[0].dateTime.day)
+    : DateTime(0);
+  devtools.log('----');
+  for (int i = 0; i < meals.length; i++) {
     final meal = meals[i];
     final product = productsMap?[meal.productQuantity.productId];
     final mealDate = DateTime(meal.dateTime.year, meal.dateTime.month, meal.dateTime.day);
+    devtools.log('mealDate: $mealDate');
     
     if (lastHeader.isAfter(mealDate)) {
       // print error
       devtools.log('MealList: meals are not sorted by date');
-    } else if (lastHeader.isBefore(mealDate)) {
+    } else if (lastHeader.isAfter(mealDate)) {
+      children.add(getDateStrip(context, lastHeader));
       lastHeader = mealDate;
-      // Convert date to natural string
-      String text;
-      int relativeDays = mealDate.difference(DateTime.now()).inDays.abs();
-      if (relativeDays <= 7) {
-        text = "${relativeDaysNatural(mealDate)} (${conditionallyRemoveYear(context, [mealDate], showWeekDay: true)[0]})";
-      } else {
-        text = conditionallyRemoveYear(context, [mealDate], showWeekDay: true)[0];
-      }
-      children.add(
-        Container(
-          decoration: const BoxDecoration(
-            color: Color.fromARGB(255, 200, 200, 200),
-          ),
-          child: Center(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(vertical: 5),
-              child: Text(text),
-            ),
-          ),
-        ),
-      );
+    } else {
+      // add divider
+      children.add(getHorizontalLine());
     }
     
     var unitName = unitToString(meal.productQuantity.unit);
@@ -92,24 +81,36 @@ Widget getMealTiles(BuildContext context, Map<int, Product>? productsMap, List<M
         dense: true,
         minVerticalPadding: 0,
         visualDensity: const VisualDensity(vertical: -2),
-        trailing: IconButton(
-          icon: const Icon(Icons.edit),
-          onPressed: () => {},
-        ),
+        trailing: // three dot menu
+          PopupMenuButton(
+            itemBuilder: (context) => [
+              PopupMenuItem(
+                value: 0,
+                child: const Text('Edit'),
+              ),
+              PopupMenuItem(
+                value: 1,
+                child: const Text('Delete'),
+              ),
+            ],
+            onSelected: (int value) {
+              if (value == 0) {
+                // edit
+              } else if (value == 1) {
+                // delete
+                dataService.deleteMeal(meal.id);
+              }
+            },
+          ),
       ),
     );
-    if (i < meals.length - 1) {
-      children.add(const Divider(
-        indent: 10,
-        endIndent: 10,
-        height: 1,
-      ));
-    }
   }
   
-  return Column(
-    children: children,
-  );
+  if (meals.isNotEmpty) {
+    children.add(getDateStrip(context, lastHeader));
+  }
+  
+  return children;
 }
 
 Widget getHorizontalLine() =>
@@ -118,3 +119,26 @@ Widget getHorizontalLine() =>
     endIndent: 10,
     height: 1,
   );
+
+Widget getDateStrip(BuildContext context, DateTime dateTime) {
+  // Convert date to natural string
+      String text;
+      int relativeDays = dateTime.difference(DateTime.now()).inDays.abs();
+      if (relativeDays <= 7) {
+        text = "${relativeDaysNatural(dateTime)} (${conditionallyRemoveYear(context, [dateTime], showWeekDay: true)[0]})";
+      } else {
+        text = conditionallyRemoveYear(context, [dateTime], showWeekDay: true)[0];
+      }
+  
+  return Container(
+    decoration: const BoxDecoration(
+      color: Color.fromARGB(255, 200, 200, 200),
+    ),
+    child: Center(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 5),
+        child: Text(text),
+      ),
+    ),
+  );
+}
